953ebf9ecdf0e119550c3f01d27ac8a6b4278804
Julian Bäume <julian@svg4all.de>
2012-09-10 16:17:09 +0200

    update dummy app
    use new version of devise, fixes a warning.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -44,17 +44,6 @@ ActiveRecord::Schema.define(:version => 20120803150826) do
     t.integer "position"
   end
 
-  create_table "file_assets", :force => true do |t|
-    t.string   "file"
-    t.string   "content_type"
-    t.integer  "file_size"
-    t.datetime "updated_at"
-    t.datetime "created_at"
-    t.string   "author"
-    t.string   "source"
-    t.string   "license"
-  end
-
   create_table "labelings", :force => true do |t|
     t.integer "classification_id"
     t.integer "label_id"
@@ -85,16 +74,13 @@ ActiveRecord::Schema.define(:version => 20120803150826) do
     t.integer "clipboard_id"
   end
 
-  create_table "urls", :force => true do |t|
-    t.string "url"
-  end
-
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
