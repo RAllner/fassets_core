1311c4a9ffdb073b90aa654b640a83c5d683b261
Julian Bäume <julian@svg4all.de>
2012-10-13 20:35:47 +0200

    make assets/new work without fancybox
    this is just some adjustment for the JS and some css cleanups. Now it’s
    possible to create new assets without the fancybox, again.

--- app/assets/javascripts/fassets_core/add_asset_box.js
+++ app/assets/javascripts/fassets_core/add_asset_box.js
@@ -11,8 +11,6 @@ $(document).ready(function(){
     $.fancybox.showActivity();
     var f_width = $(window).width()*0.8;
     var f_height = $(window).height()*0.8;
-    $("#add_asset_content").css("left",$("#catalog_list").width()+10);
-    $("#add_asset_content").css("width",$("#fancybox-content").width()-$("#catalog_list").width()-30-$("#facets").width());
     $.ajaxSetup({cache: false});
     $.get('/assets/new', { content_only: true }, function(data) {
       $.fancybox({
@@ -23,7 +21,6 @@ $(document).ready(function(){
         height: f_height,
         onComplete: function(){
           $("#fancybox-content").data("box-type","add_asset");
-          adjust_links();
           $.fancybox.resize();
         }
       });
@@ -43,40 +40,42 @@ $(document).ready(function(){
       }
       break;
     }
-   });
-    var adjust_links = function(){
-      $("#fancybox-content li.asset_type").click(function(event){
-        event.preventDefault();
-        $.fancybox.showActivity();
-        asset_type = $(event.target).data("asset-type");
-        $("#fancybox-content").load("/assets/new", {type: asset_type, content_only: true }, function() {
-          adjust_links();
-        });
-        $.fancybox.resize();
-        $.fancybox.hideActivity();
+  });
+  $(document).ajaxComplete(function() {
+    adjust_links();
+  });
+  var adjust_links = function() {
+    $("#add_asset_sidebar li.asset_type").click(function(event){
+      event.preventDefault();
+      $.fancybox.showActivity();
+      asset_type = $(event.target).data("asset-type");
+      $("div.fassets_core.new").load("/assets/new", {type: asset_type, content_only: true });
+      $.fancybox.resize();
+      $.fancybox.hideActivity();
+    });
+    $("#fancybox-content .asset_create_button").click(function(event){
+      event.preventDefault();
+      $.fancybox.showActivity();
+      var action = $("#add_asset_content form").attr("action");
+      $.post(action, $("#add_asset_content form").serialize(), function(data){
+        $("#fancybox-content #add_asset_content").load(data[0].edit_box_url+"?type="+data[0].content_type);
       });
-      $("#fancybox-content .asset_create_button").click(function(event){
-        event.preventDefault();
-        $.fancybox.showActivity();
-        var action = $("#add_asset_content form").attr("action");
-        $.post(action, $("#add_asset_content form").serialize(), function(data){
-          $("#fancybox-content #add_asset_content").load(data[0].edit_box_url+"?type="+data[0].content_type);
-        });
-        reload_tray();
-        $.fancybox.resize();;
-        $.fancybox.hideActivity();
-      });
-      $("#fancybox-content .asset_submit_button").click(function(event){
-        event.preventDefault();
-        $.fancybox.showActivity();
-        //var token = encodeURIComponent(AUTH_TOKEN)
-        var asset_type = $(event.target).data("asset-type");
-        reload_tray();
-        $.fancybox.hideActivity();
-      });
-      $("form.edit_classification input[type=submit][value=Save]").hide();
-    };
+      reload_tray();
+      $.fancybox.resize();;
+      $.fancybox.hideActivity();
+    });
+    $("#fancybox-content .asset_submit_button").click(function(event){
+      event.preventDefault();
+      $.fancybox.showActivity();
+      //var token = encodeURIComponent(AUTH_TOKEN)
+      var asset_type = $(event.target).data("asset-type");
+      reload_tray();
+      $.fancybox.hideActivity();
+    });
+    $("form.edit_classification input[type=submit][value=Save]").hide();
+  };
 
+  adjust_links();
   $("#new_asset_link").click(function(event){
     event.preventDefault();
     show_asset_box();
