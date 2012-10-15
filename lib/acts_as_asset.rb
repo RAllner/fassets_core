b59e7e39b59d03437803f0182e0ca511dbbcb7c4
Julian Bäume <julian@svg4all.de>
2012-10-15 16:17:07 +0200

    cleanup acts_as_asset
    I think, these methods aren’t needed any longer. So I want to remove
    them for 0.3.0 and provide specs of how acts_as_asset should work. Once
    the specs are done, there should be consensus about the behaviour of
    acts_as_asset.

--- lib/acts_as_asset.rb
+++ lib/acts_as_asset.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module Acts
     module Asset
       def self.included(base)
-        base.extend(ClassMethods)  
+        base.extend(ClassMethods)
       end
       module ClassMethods
         def acts_as_asset
@@ -15,19 +15,12 @@ module ActiveRecord
         def name
           asset.name
         end
-        def class_underscore
-          self.class.to_s.underscore
-        end
         def media_type
           "generic"
         end
         def icon
           "/images/#{media_type}.png"
         end
-        protected
-          def put_on_tray
-            tray_positions.create(:user_id => self.user_id)
-          end
       end
     end
   end
